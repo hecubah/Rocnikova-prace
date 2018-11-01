885e1080663ca0798cf7a16adcd1f7a5978507b3
hecubah <herzir232@seznam.cz>
2018-11-01 19:42:09 +0100

    Update pi_Greg.py

--- pi_Greg.py
+++ pi_Greg.py
@@ -1,25 +1,14 @@
 r=1
 
-O=8*r
-S=4*r**2
+O=4*r**2 # plocha opsaneho
+V=2*r**2 # plocha vepsaneho
 n=4
 
-print("Opsany n-uhelnik")
+print("Dvojice n-uhelniku")
 for i in range(26):
-  print("{}uhelnik: {} < pi < {}".format(n, S/2, O/(2*r)))  #proc S/2, spravně S/r**2
-  SS=(S*O)**0.5
-  O=(2*S*O)/(S+SS)
-  S=SS
+  print("{} uhelnik: {} < pi < {}".format(n, V/r**2, O/r**2))
+  VV=(V*O)**0.5
+  O=(2*V*O)/(V+VV)
+  V=VV
   n*=2
 
-o=4*(2**0.5)*r
-s=2*r**2
-k=4
-
-print("Vepsany n-uhelnik")
-for i in range(26):
-  print("{}uhelnik: {} < pi < {}".format(k, s/2, o/(2*r)))  #nefunguje
-  ss=(s*o)**0.5
-  o=(2*s*o)/(S+SS)
-  s=ss
-  k*=2
